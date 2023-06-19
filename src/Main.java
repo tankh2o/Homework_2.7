3c114dd2a87509bfc989888b7ac737d341c945e3
TanY <tankh2o@yandex.ru>
2023-06-19 15:22:06 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        tusk1();
-        tusk2();
-        tusk3();
-        tusk4();
+        List<Integer> nums = new ArrayList<>(List.of(6, 1, 1, 2, 2, 3, 6, 5, 3, 4, 4, 5));
 
-        oddNumbers(List.of(1,2,3,2,3,1,4,4));
+        System.out.println(" Задание 1");
+        oddNumbers(nums);
+        System.out.println("\n Задание 2");
+        evenNumbers(nums);
+
+        List<String> words = new ArrayList<>(List.of("до", "ми", "соль", "до", "до", "соль", "ми", "до", "соль", "соль", "ми", "до"));
+        System.out.println("\n Задание 3");
+        deletedDuplicatesWords(words);
+        System.out.println("\n Задание 4");
+        numberDuplicates(words);
     }
-
-    public static void tusk1() {
-        /*Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
-                Код должен работать с любой последовательностью и объемом списка чисел.*/
-        List<Integer> nums = new ArrayList<>(List.of(1, 2, 2, 3, 4, 1, 3, 4));
+    public static void oddNumbers(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 != 0) {
+                System.out.print(nums.get(i) + " ");
+            }
+        }
+    }
+    public static void evenNumbers(List<Integer> nums) {
+        deletedDuplicatesNums(nums);
+        Collections.sort(nums);
         for (int i = 0; i < nums.size(); i++) {
             if (nums.get(i) % 2 == 0) {
-                nums.remove(i);
+                System.out.print(nums.get(i) + " ");
             }
-            System.out.println(nums.get(i));
         }
     }
-
-    public static void oddNumbers (List<Integer> nums) {
+    public static void deletedDuplicatesNums(List<Integer> nums) {
         for (int i = 0; i < nums.size(); i++) {
-            //for (int k = 0; k < nums.size(); k++) {
-            if (nums.get(i) % 2 != 0) {
-                System.out.println(nums.get(i));
+            for (int k = 0; k < nums.size() && nums.get(i) != null; k++) {
+                if (nums.get(i).equals(nums.get(k)) && i != k) {
+                    nums.remove(k);
+                }
             }
         }
     }
-    public static void tusk2() {
-        /*Напишите код, с помощью которого можно напечатать только четные числа без повторений
-        в порядке возрастания. Код должен работать с любой последовательностью и объемом списка чисел.*/
+    public static void deletedDuplicatesWords(List<String> words) {
+        Set<String> removeDoubles = new HashSet<>(words);
+        System.out.print(removeDoubles);
     }
-    public static void tusk3() {
-        /*Напишите код, который выводит в консоль все уникальные слова из списка слов,
-                в котором могут встречаться дубли. Код должен работать с любой последовательностью и объемом списка слов.
-        В качестве отладочной информации возьмите произвольный набор слов или текст, в котором встречаются повторения.*/
-    }
-    public static void tusk4() {
-        /*Напишите код, который выводит в консоль количество дублей для каждого уникального слова.
-                Код должен работать с любой последовательностью и объемом списка слов.*/
+    public static void numberDuplicates(List<String> words) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (String w : words) {
+            if (resultMap.containsKey(w)) {
+                resultMap.put(w, (resultMap.get(w) + 1));
+            } else {
+                resultMap.put(w, 1);
+            }
+        }
+        for (String w1 : resultMap.keySet()) {
+            System.out.println("Слово " + w1 + " встретилось " + resultMap.get(w1) + " раз.");
+        }
     }
 }
